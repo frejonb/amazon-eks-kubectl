4633b56e23811b68f4db8551783c26893751a328
Fernando Rejon Barrera <39321865+frejonb@users.noreply.github.com>
2019-08-07 11:32:52 +0200

    Update kubectl.sh

--- kubectl.sh
+++ kubectl.sh
@@ -1,39 +1,39 @@
 #!/bin/sh
 
 if [ -z "$CLUSTER" ]; then
-    echo "Please provide an Amazon EKS cluster name by populating the CLUSTER environment variable"
-    echo "e.g."
-    echo "docker run -e CLUSTER=mycluster maddox/kubectl get pods"
-    exit 1
+    # Run without kube config, if using service account for example
+    "$@"
+else
+    # Write a ~/.kube/config file, using the AWS CLI to fetch the necessary parameters
+    mkdir -p ~/.kube
+    (cat > ~/.kube/config) <<EOF
+    apiVersion: v1
+    clusters:
+    - cluster:
+        server: $(aws eks describe-cluster --name ${CLUSTER} | jq -r .cluster.endpoint) 
+        certificate-authority-data: $(aws eks describe-cluster --name ${CLUSTER} | jq -r .cluster.certificateAuthority.data) 
+      name: kubernetes
+    contexts:
+    - context:
+        cluster: kubernetes
+        user: aws
+      name: aws
+    current-context: aws
+    kind: Config
+    preferences: {}
+    users:
+    - name: aws
+      user:
+        exec:
+          apiVersion: client.authentication.k8s.io/v1alpha1
+          command: aws-iam-authenticator
+          args:
+            - "token"
+            - "-i"
+            - ${CLUSTER}
+    EOF
+    
+    "$@"
 fi
 
-# Write a ~/.kube/config file, using the AWS CLI to fetch the necessary parameters
-mkdir -p ~/.kube
-(cat > ~/.kube/config) <<EOF
-apiVersion: v1
-clusters:
-- cluster:
-    server: $(aws eks describe-cluster --name ${CLUSTER} | jq -r .cluster.endpoint) 
-    certificate-authority-data: $(aws eks describe-cluster --name ${CLUSTER} | jq -r .cluster.certificateAuthority.data) 
-  name: kubernetes
-contexts:
-- context:
-    cluster: kubernetes
-    user: aws
-  name: aws
-current-context: aws
-kind: Config
-preferences: {}
-users:
-- name: aws
-  user:
-    exec:
-      apiVersion: client.authentication.k8s.io/v1alpha1
-      command: aws-iam-authenticator
-      args:
-        - "token"
-        - "-i"
-        - ${CLUSTER}
-EOF
 
-"$@"
